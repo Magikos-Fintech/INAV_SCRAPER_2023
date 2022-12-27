600032837636784cfa8167f0cea48d781c48dcbb
asnim-mt20 <121017539+asnim-mt20@users.noreply.github.com>
2022-12-27 13:06:26 +0530

    Updated inavFetcher.js

--- inavFetcher.js
+++ inavFetcher.js
@@ -4,47 +4,7 @@ const { iifl_nav_selector, iifl_date_selector, close_popup_btn } = require('./se
 const { executablePath } = require('puppeteer')
 
 const getInavData = function (sheetData) {
-    // sheetData = [
-    //     {
-    //         id: '1',
-    //         symbol: 'IVZINNIFTY-NSE',
-    //         URL_ID: '99615593',
-    //         url: 'https://www.indiainfoline.com/mutualfunds/Invesco-India-Nifty-50-ETF/99615593',
-    //         inav: '',
-    //         asOnDate: ''
-    //     },
-    //     {
-    //         id: '2',
-    //         symbol: 'IBMFNIFTY-NSE',
-    //         URL_ID: '313646410',
-    //         url: 'https://www.indiainfoline.com/mutualfunds/Indiabulls-Nifty50-Exchange-Traded-Fund/313646410',
-    //         inav: '',
-    //         asOnDate: ''
-    //     },
-    //     {
-    //         id: '3',
-    //         symbol: 'DSPN50ETF-BSE',
-    //         URL_ID: '330810249',
-    //         url: 'https://www.indiainfoline.com/mutualfunds/DSP-Nifty-50-ETF/330810249',
-    //         inav: '',
-    //         asOnDate: ''
-    //     },
-    //     {
-    //         id: '4',
-    //         symbol: 'NETF-NSE',
-    //         URL_ID: '303862944',
-    //         url: 'https://www.indiainfoline.com/mutualfunds/Tata-Nifty-50-Exchange-Traded-Fund/303862944',
-    //         inav: '',
-    //         asOnDate: ''
-    //     },
-    //     {
-    //         id: '5',
-    //         symbol: 'MOM50-NSE',
-    //         URL_ID: '89046650',
-    //         url: 'https://www.indiainfoline.com/mutualfunds/Motilal-Oswal-Nifty-50-ETF/89046650',
-    //         inav: '',
-    //         asOnDate: ''
-    //     }]
+   
     return new Promise(async (resolve, reject) => {
         try {
             //to launch a browser instance
@@ -54,7 +14,6 @@ const getInavData = function (sheetData) {
             const browser = await puppeteer.launch({
                 //true to avoid opening the browser everytime a request is sent
                 headless: true,
-<<<<<<< HEAD
                 args: ["--no-sandbox", "--disable-setuid-sandbox"],
                 // args: [`--window-size=1920,1080`],
                 // defaultViewport: {
@@ -62,13 +21,6 @@ const getInavData = function (sheetData) {
                 //     height: 1080
                 // },
                 executablePath: executablePath()
-=======
-                args: [`--window-size=1920,1080`],
-                defaultViewport: {
-                    width: 1920,
-                    height: 1080
-                }
->>>>>>> 1146913901fc9c7aa47a0cb37de3ac974f39d199
             });
 
             const page = await browser.newPage();
