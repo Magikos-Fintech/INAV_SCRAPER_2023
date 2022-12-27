109995adbe90b2a482217dd412136d2aa2532197
asnim-mt20 <asnim@magikostech.com>
2022-12-27 17:46:07 +0530

    added money control flow

--- inavFetcher.js
+++ inavFetcher.js
@@ -1,10 +1,10 @@
 var puppeteer = require('puppeteer-extra');
-const { iifl_nav_selector, iifl_date_selector, close_popup_btn } = require('./setup')
+const { iifl_nav_selector, iifl_date_selector, dateSelector,amtSelector } = require('./setup')
 
 const { executablePath } = require('puppeteer')
 
 const getInavData = function (sheetData) {
-   
+
     return new Promise(async (resolve, reject) => {
         try {
             //to launch a browser instance
@@ -13,7 +13,9 @@ const getInavData = function (sheetData) {
 
             const browser = await puppeteer.launch({
                 //true to avoid opening the browser everytime a request is sent
+
                 headless: true,
+
                 args: ["--no-sandbox", "--disable-setuid-sandbox"],
                 // args: [`--window-size=1920,1080`],
                 // defaultViewport: {
@@ -26,14 +28,15 @@ const getInavData = function (sheetData) {
             const page = await browser.newPage();
             const responseArray = []
             for (var i = 0; i < sheetData.length; i++) {
-                var iifl_links = sheetData[i].url;
+                var links = sheetData[i].url;
                 var id = sheetData[i].id;
                 try {
-                    await page.goto(`${iifl_links}`);
-                    console.log(iifl_links)
-                    const element = await page.$(`${close_popup_btn}`);
-                    if (element) {
-                        await page.$eval(`${close_popup_btn}`, el => el.click());
+                    if (links.indexOf('www.indiainfoline.com') !== -1) {
+                        var iifl_links = links
+                        await page.goto(`${iifl_links}`);
+                        console.log(iifl_links)
+
+                        //await page.$eval(`${close_popup_btn}`, el => el.click());
                         //await page.click(`${close_popup_btn}`)
 
                         await page.waitForSelector(`${iifl_nav_selector}`)
@@ -43,19 +46,36 @@ const getInavData = function (sheetData) {
 
                         var obj = [id, nav, date]
                         responseArray.push(obj)
-                    } else {
+                    }
+                } catch (e) {
+                    console.log("Something went wrong while scraping indiainfoline.com! " + e)
+                }
+                try {
+                    if (links.indexOf('www.moneycontrol.com') !== -1) {
+                        var moneyControlLinks = links
+                        console.log(moneyControlLinks)
+                        await page.goto(`${moneyControlLinks}`);
 
-                        await page.waitForSelector(`${iifl_nav_selector}`)
-                        await page.waitForSelector(`${iifl_date_selector}`)
-                        let nav = await page.$eval(`${iifl_nav_selector}`, el => el.innerText)
-                        let date = await page.$eval(`${iifl_date_selector}`, date => date.innerText)
+                        await page.waitForSelector(`${amtSelector}`)
+                        await page.waitForSelector(`${dateSelector}`)
+                        let amount = await page.$eval(`${amtSelector}`, el => el.innerText)
+                        let date = await page.$eval(`${dateSelector}`, date => date.innerText)
 
-                        var obj = [id, nav, date]
+                        amount = amount.split(' ')[1]
+                        date = date.split('(as on ')[1].split(')')[0]
+                        date = date.split(/th|st|nd|rd/).join('')
+                        let formattedDate = new Date(date).toLocaleDateString('en-GB', {
+                            day: '2-digit',
+                            month: 'short',
+                            year: 'numeric'
+                        }).replace(/ /g, '-');
+
+                        var obj = [id, amount, formattedDate]
                         responseArray.push(obj)
                     }
 
                 } catch (e) {
-                    console.log("Something went wrong while scraping indiainfoline.com! "+ e)
+                    console.log("Something went wrong while scraping moneycontrol.com! " + e)
                 }
             }
             //await browser.close()
--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
-// const amtSelector="#mc_content > div > section.clearfix.section_one > div > div.common_left > div:nth-child(3) > div.left_section > div.top_section > ul > li > div.leftblok > span.amt"
-// const dateSelector="#mc_content > div > section.clearfix.section_one > div > div.common_left > div:nth-child(3) > div.left_section > div.top_section > ul > li > div.leftblok > div.grayvalue"
+const amtSelector="#mc_content > div > section.clearfix.section_one > div > div.common_left > div:nth-child(3) > div.left_section > div.top_section > ul > li > div.leftblok > span.amt"
+const dateSelector="#mc_content > div > section.clearfix.section_one > div > div.common_left > div:nth-child(3) > div.left_section > div.top_section > ul > li > div.leftblok > div.grayvalue"
 
 // const bsl_nav_selector="#tabIndex0 > div > div > div.dvRow2 > div > ul.type2 > li:nth-child(2)"
 // const hdfc_date_selector="#react-tabs-5 > div > div:nth-child(2) > div.drupal > table > tbody > tr > td:nth-child(2)"
@@ -14,3 +14,6 @@ const close_popup_btn="body > app-root > app-new-lead-grid-form > div > div > bu
 exports.iifl_nav_selector=iifl_nav_selector;
 exports.iifl_date_selector=iifl_date_selector;
 exports.close_popup_btn=close_popup_btn;
+
+exports.amtSelector=amtSelector;
+exports.dateSelector=dateSelector;
